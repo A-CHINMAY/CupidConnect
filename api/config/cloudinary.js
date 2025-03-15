6e8f4c47c799109ca9cb962fccac226d00957a4b
A-CHINMAY <a.chinmay2005@gmail.com>
2025-03-15 16:35:59 +0530

    updated

--- api/config/cloudinary.js
+++ api/config/cloudinary.js
@@ -2,10 +2,6 @@ import { v2 as cloudinary } from "cloudinary";
 import dotenv from "dotenv";
 
 dotenv.config();
-console.log("Cloudinary ENV variables:");
-console.log("CLOUD_NAME:", process.env.CLOUDINARY_CLOUD_NAME);
-console.log("API_KEY:", process.env.CLOUDINARY_API_KEY ? "Exists" : "Missing");
-console.log("API_SECRET:", process.env.CLOUDINARY_API_SECRET ? "Exists" : "Missing");
 cloudinary.config({
 	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
 	api_key: process.env.CLOUDINARY_API_KEY,
